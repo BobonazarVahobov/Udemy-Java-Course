e499134a9b839094b1245c38c493e8221948ccf6
BobonazarVahobov <bobonazarvahobov@gmail.com>
2021-09-30 15:16:31 +0900

    01-Section added

---
+++ 01-Section/02-BarkingDog.java
@@ -0,0 +1,10 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay) {
+      if(!isBarking || hourOfDay < 0 || hourOfDay > 23 || (hourOfDay > 7 && hourOfDay < 23)) {
+          return false;
+      }
+        return true;
+
+
+}
+}
